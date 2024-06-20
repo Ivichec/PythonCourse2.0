6636be856aa709422469aa1587e9722be0f6fad2
Ivan Checa <124150320+Ivichec@users.noreply.github.com>
2024-06-20 13:54:15 +0200

    .

--- cursoPython/ProyectosDjango/pythonProjectDjango/EjercicioCRUD/EjercicioCRUD/asgi.py
+++ cursoPython/ProyectosDjango/pythonProjectDjango/EjercicioCRUD/EjercicioCRUD/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EjercicioCRUDIvan project.
+ASGI config for EjercicioCRUD project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EjercicioCRUDIvan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EjercicioCRUD.settings')
 
 application = get_asgi_application()
--- cursoPython/ProyectosDjango/pythonProjectDjango/EjercicioCRUD/EjercicioCRUD/urls.py
+++ cursoPython/ProyectosDjango/pythonProjectDjango/EjercicioCRUD/EjercicioCRUD/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for EjercicioCRUDIvan project.
+URL configuration for EjercicioCRUD project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
