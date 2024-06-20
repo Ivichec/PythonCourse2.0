851dfc1ca7e9c8915688b024141b8adda4533240
Ivan Checa <124150320+Ivichec@users.noreply.github.com>
2024-06-20 13:45:53 +0200

    .

--- cursoPython/ProyectosDjango/pythonProjectDjango/EjercicioCRUDIvan/EjercicioCRUD/urls.py
+++ cursoPython/ProyectosDjango/pythonProjectDjango/EjercicioCRUDIvan/EjercicioCRUD/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for EjercicioCRUD project.
+URL configuration for EjercicioCRUDIvan project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
