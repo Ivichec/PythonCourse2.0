9415b540974a3067737897a044033033c85a5856
Ivan Checa <124150320+Ivichec@users.noreply.github.com>
2024-06-25 10:23:15 +0200

    .

---
+++ cursoPython/EjProyectoWebApi/Ejemplo1.py
@@ -0,0 +1,17 @@
+import requests
+
+import json
+
+api_url = "https://apiempleadosspgs.azurewebsites.net/api/Empleados/7369"
+
+#capturamos la respuesta
+
+response = requests.get(api_url)
+
+#convertimos la respuesta a objeto diccionario
+
+empleado = response.json()
+
+print(empleado)
+
+print(empleado["apellido"])
